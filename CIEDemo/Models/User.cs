902719b74b2db3c113c23c90f9ed669225b60aad
Luke Irvin <thelukester145@gmail.com>
2021-06-10 09:17:08 -0500

    Add project files.

---
+++ CIEDemo/Models/User.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CIEDemo.Models
+{
+    public class User
+    {
+        [Key]
+        public long ID { get; set; }
+
+        [Column(TypeName = "nvarchar(50)")]
+        public string FirstName { get; set; }
+
+        [Column(TypeName = "nvarchar(50)")]
+        public string LastName { get; set; }
+
+    }
+}
